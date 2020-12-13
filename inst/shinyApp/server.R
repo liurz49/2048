8c550c87a624eef36bf5ce3b86216bf75c3970ec
Rongzi Liu <liu.r@ufl.edu>
2020-12-13 02:10:53 -0500

    update

--- inst/shinyApp/server.R
+++ inst/shinyApp/server.R
@@ -10,28 +10,28 @@ server <- function(input, output, session) {
             e <<- create(e)
             # bg()
             gp2 <- bg.matrix(e)
-            
-            
+
+
             output$plot <- renderPlot({
                 gp2
             })
-            
+
         }
     })
-    
+
     resource <- system.file("start.gif", package = "play2048")
     # resource = '../../resource' = system.file('resource', package = 'play2048')
     # print(resource)
-    
+
     output$plot <- renderImage({
         # print( paste(resource, '/start.gif'))
-        
+
         list(src = resource, contentType = "image/gif")
-    })
-    
-    
+        }, deleteFile = FALSE)
+
+
     observeEvent(input$startGame, {
-        
+
         active(TRUE)
         # e <- new.env()
         e <<- e.init()
@@ -50,12 +50,12 @@ server <- function(input, output, session) {
         # 'm') )
         shinyalert("Oops!", "Game Over", type = "info", imageWidth = 50, imageHeight = 40)
     })
-    
+
     observeEvent(input$reset, {
         active(FALSE)
         output$plot <- renderImage({
             list(src = resource, contentType = "image/gif")
-        })
+        }, deleteFile = FALSE)
     })
 }
 
